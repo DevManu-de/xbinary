ea65164fc9e01b55bc81a9f85881001bb397698f
Manuel Bertele <manuel.bertele2003@gmail.com>
2021-09-26 02:18:47 +0200

    Fixed wrong number of digits

--- include/xbinary.h
+++ include/xbinary.h
@@ -8,9 +8,9 @@
 #define rotr(x, y) ((x >> y) | (x << (sizeof(x) * 8 - y)))
 
 #define __to_bin__(x)   __uint128_t __x__ = x;                                      \
-                        char __arr__[sizeof(x) * 8] = {'\0'};                       \
+                        char __arr__[sizeof(x) * 9] = {'\0'};                       \
                         int16_t __i__;                                              \
-                        for (__i__ = sizeof(x) * 8 - 2; __i__ >= 0; --__i__) {      \
+                        for (__i__ = sizeof(x) * 8 - 1; __i__ >= 0; --__i__) {      \
                             __arr__[__i__] = (__x__ & 1) + '0';                     \
                             __x__ >>= 1;                                            \
                         }                                                           \
